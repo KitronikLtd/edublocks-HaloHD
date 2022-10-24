594cf89ded13cc896faf26ae56718dab76384424
lukecross05 <116554470+lukecross05@users.noreply.github.com>
2022-10-24 15:12:07 +0100

    added functions

--- generators.ts
+++ generators.ts
@@ -1,28 +1,40 @@
-Blockly.Python['import_move_motor'] = function(block) {
-  var code = 'from KitronikMOVEMotor import *\n';
+Blockly.Python['import_neopixel'] = function(block) {
+  var code = 'from neopixel import NeoPixel\n';
+  return code;
+};
+Blockly.Python['neopixel_init'] = function(block) {
+  var variable_led = Blockly.Python.variableDB_.getName(block.getFieldValue('Pixels'), Blockly.Variables.NAME_TYPE);
+  var code = variable_led + ' = NeoPixel(pin8, LEDS_ON_HALO)\n';
   return code;
 };
 
-Blockly.Python['move_motor_init'] = function(block) {
-  var variable_buggy = Blockly.Python.variableDB_.getName(block.getFieldValue('buggy'), Blockly.Variables.NAME_TYPE);
-  var code = variable_buggy + ' = MOVEMotor()\n';
+Blockly.Python['set_led'] = function(block) {
+  var variable_lednum = Blockly.Python.variableDB_.getNumber(block.getFieldValue('Pixels'), Blockly.Variables.NAME_TYPE);
+  var variable_ledcol = block.getFieldValue('Pixels');
+  if ledcol == 'red':
+    var code = variable_led + '['+variable_lednum+'] = (255, 0, 0)\n';
+  elif ledcol == 'green':
+    var code = variable_led + '['+variable_lednum+'] = (0, 255, 0)\n';
+  elif ledcol == 'blue':
+    var code = variable_led + '['+variable_lednum+'] = (0, 0, 255)\n';
   return code;
 };
 
-Blockly.Python['move_motor_direction'] = function(block) {
-  var variable_buggy = Blockly.Python.variableDB_.getName(block.getFieldValue('buggy'), Blockly.Variables.NAME_TYPE);
-  var dropdown_motor_side = block.getFieldValue('motor_side');
-  var dropdown_motor_direction = block.getFieldValue('motor_direction');
-  var value_motor_speed = Blockly.Python.valueToCode(block, 'motor_speed', Blockly.Python.ORDER_ATOMIC);
-  if (dropdown_motor_direction == "backward")
-    value_motor_speed *= -1;
-  var code = variable_buggy+ '.' + dropdown_motor_side + '(' + value_motor_speed + ')\n';
+Blockly.Python['set_all_leds'] = function(block) {
+  var code = "";
+    var variable_ledcol = block.getFieldValue('Pixels');
+  for(var i=0;i>60; i++)
+    if ledcol == 'red':
+      code += variable_led + '['+i+'] = (255, 0, 0)\n';
+    elif ledcol == 'green':
+      code += = variable_led + '['+i+'] = (0, 255, 0)\n';
+    elif ledcol == 'blue':
+      code += = variable_led + '['+i+'] = (0, 0, 255)\n';
   return code;
 };
 
 
-Blockly.Python['move_motor_stop'] = function(block) {
-  var variable_buggy = Blockly.Python.variableDB_.getName(block.getFieldValue('buggy'), Blockly.Variables.NAME_TYPE);
-  var code = variable_buggy + '.StopMotors()\n';
+Blockly.Python['show_leds'] = function(block) {
+  var code = 'zip_halo_display.show()\n';
   return code;
 };
