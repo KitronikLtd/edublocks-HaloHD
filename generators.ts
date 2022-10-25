c118317f9c894d948c7bb0ad10ffeb8186d2e040
lukecross05 <116554470+lukecross05@users.noreply.github.com>
2022-10-25 14:22:02 +0100

    Update generators.ts

--- generators.ts
+++ generators.ts
@@ -9,7 +9,7 @@ Blockly.Python['neopixel_init'] = function(block) {
   var variable_led = Blockly.Python.valueToCode(block, 'Pixels', Blockly.Python.ORDER_ATOMIC);
   var code = 'from microbit import pin8\n';
   code += 'LEDS_ON_HALO=60\n';
-  code += variable_led + ' = NeoPixel(pin8, LEDS_ON_HALO\n';
+  code += variable_led + 'NeoPixel(pin8, LEDS_ON_HALO)\n';
   return code;
 };
 
