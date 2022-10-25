d493692234af5959adfd6b96fdc528fb41c266c4
lukecross05 <116554470+lukecross05@users.noreply.github.com>
2022-10-25 15:35:51 +0100

    Update generators.ts

--- generators.ts
+++ generators.ts
@@ -41,7 +41,7 @@ Blockly.Python['set_led'] = function(block) {
 };
 
 Blockly.Python['set_all_leds'] = function(block) {
-  var code = "for i in range(60);\n{";
+  var code = "for i in range(60);\n{\n";
   var variable_led = Blockly.Python.variableDB_.getName(block.getFieldValue('Pixels'), Blockly.Variables.NAME_TYPE);
   var variable_ledcol = block.getFieldValue('Colour');
   if (variable_ledcol == 'red')
@@ -56,7 +56,7 @@ Blockly.Python['set_all_leds'] = function(block) {
     {
       code += '     ' + variable_led + '[i] = (0, 0, 255)\n';
     }
-  code +="}";
+  code +="}\n";
   return code;
 };
 
